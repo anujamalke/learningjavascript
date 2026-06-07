132e659159b94ce8068e481316fe89e19b62ede5
Anuja Malke <anuja930718@gmail.com>
2026-06-07 16:07:35 +0530

    vars and consts

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("This is my first Javascript program!")
+console.log("This is my second Javascript program!")
+
+a=3;
+console.log(a);
+console.log(typeof a);
+
+///// Variables and constants ////////
+
+let emailAddress = "amalke@gmail.com";
+const fName = "Anuja";
+console.log(emailAddress);
+console.log(typeof emailAddress);
+
+console.log(fName);
+console.log(typeof fName);
